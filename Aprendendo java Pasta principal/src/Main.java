b7f5b6c1de5f7e1d0b45763ea00591956e202b63
Vinicius Cataldi <viniciusacataldi@gmail.com>
2025-05-15 15:10:14 -0300

    Organizando

---
+++ Aprendendo java Pasta principal/src/Main.java
@@ -0,0 +1,101 @@
+public class Main {
+    public static void main(String[] args) {
+        // 1. Declarando variáveis--------------------------------------------------------------------------------
+
+
+        // TIPO DE DADO + IDENTIFICAÇÃO + VALOR ATRIBUIDO
+        
+        String nome = "Josie Da Cruz";
+
+        // CAMMEL CASE (não delcarar variavel começando com letra maiuscula porem caso for duas palavras a segunda começa com letra maiuscula EX:)
+
+        String nomeSobrenome = "Da Cruz";
+
+        // SISTEMA + SAIDA + PRINT + ( VARIAVEL )
+
+        System.out.println(nome);
+        System.out.println(nomeSobrenome);
+        
+        // 2. Tipos de variáveis--------------------------------------------------------------------------------
+
+        /* 
+
+        byte:   -128 a 127 (1 byte de informação ou 8 bits)
+        short:  -32768 a 32767 (2 bytes de informação ou 16 bits)
+        int:    -2147483648 a 2147483647 (2 bilhões) (4 bytes de informação ou 32 bits)
+        long:   -9223372036854775808 a 9223372036854775807 (8 bytes de informação ou 64 bits)
+
+        ----Números decimais----
+
+        float:  7 casas decimais – 1234567 (4 bytes de informação ou 32 bits)
+        double: 15 casas decimais – 123456789123456 (8 bytes de informação ou 64 bits)
+
+        String: vai receber textos → palavras e frases
+        char:   suporta apenas um único caracter
+
+        */
+
+        byte idade = 120;
+        short estoque = 32700;
+        int numero = 2000000000;
+
+        // Variaveis long devem terminar com L  |
+
+        long contaCorrente = 9223372036854775807L;
+
+
+        // Variaveis float devem terminar com f
+
+        float real = 10.1234567f;
+        double dolar = 1.123456789112345;
+
+        String nome2 = "Rita Duarte";
+        // Somente um caracter dentro de aspas simples
+        char primeiraLetra = 'R';
+
+        boolean botaoAtivado = true;
+        boolean botaoDesativado = false;
+
+
+        // FORMA NOVA DE DECLARAR VARIAVEIS
+        // O tipo da variavel vai ser reconhecido automaticamente apartir do tipo de dados que exitem dentro dela
+        var sobrenome = "Duarte Gomes";
+
+
+        System.out.println(idade);
+        System.out.println(estoque);
+        System.out.println(numero);
+        System.out.println(contaCorrente);
+        System.out.println(real);
+        System.out.println(dolar);
+        System.out.println(nome2);
+        System.out.println(primeiraLetra);
+        System.out.println(botaoAtivado);
+        System.out.println(botaoDesativado);
+        System.out.println(sobrenome);
+
+
+        // 3. Classes Wrappers--------------------------------------------------------------------------------
+        // 4. Mutabilidade e Imutabilidade--------------------------------------------------------------------------------
+        // 5. Comments--------------------------------------------------------------------------------
+        // 6. Concatenação--------------------------------------------------------------------------------
+        // 7. Operadores Aritméticos--------------------------------------------------------------------------------
+        // 8. Operadores Relacionais--------------------------------------------------------------------------------
+        // 9. IF e Else--------------------------------------------------------------------------------
+        // 10. Operadores Lógicos--------------------------------------------------------------------------------
+        // 11. Arrays--------------------------------------------------------------------------------
+        // 12. While--------------------------------------------------------------------------------
+        // 13. For--------------------------------------------------------------------------------
+        // 14. Switch--------------------------------------------------------------------------------
+        // 15. O que é POO--------------------------------------------------------------------------------
+        // 16. Objeto, Propriedade e Métodos--------------------------------------------------------------------------------
+        // 17. Getters e Setters--------------------------------------------------------------------------------
+        // 18. Construtores--------------------------------------------------------------------------------
+        // 19. Herança--------------------------------------------------------------------------------
+        // 20. Modificadores de Acesso--------------------------------------------------------------------------------
+        // 21. Interfaces--------------------------------------------------------------------------------
+        // 22. Conclusão--------------------------------------------------------------------------------
+        // 23. Final--------------------------------------------------------------------------------
+
+    }
+}
